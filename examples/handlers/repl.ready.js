3635f631511440f2fc44dfd909424b3e932d830a
goliatone <burgosemi@gmail.com>
2016-10-17 14:20:39 -0400

    comment on repl.ready

--- examples/handlers/repl.ready.js
+++ examples/handlers/repl.ready.js
@@ -1,5 +1,8 @@
 'use strict';
-
+/*
+ * Attach the application instance to
+ * the REPLs context.
+ */
 module.exports = function replReady(repl){
     this.logger.warn('REPL ready');
     repl.context.app = this;
