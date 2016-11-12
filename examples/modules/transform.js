4336dc7a6a746adbaa2786269ee280230de7ae1e
goliatone <burgosemi@gmail.com>
2016-11-12 15:20:42 -0500

    example: module transform use getLogger

--- examples/modules/transform.js
+++ examples/modules/transform.js
@@ -1,4 +1,6 @@
 'use strict';
 module.exports.init = function(app, config){
     console.log('Transform module', config);
+    app.transform = {hello: function(){}};
+    app.getLogger('transform').info('Transform module');
 };
