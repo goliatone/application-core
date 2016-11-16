aeb7652941b437f2fca7489d885ccba97a99c90a
goliatone <burgosemi@gmail.com>
2016-11-16 18:59:45 -0500

    example: transform module use app.resolve for pubsub dependencie

--- examples/modules/transform.js
+++ examples/modules/transform.js
@@ -1,3 +1,4 @@
+/*jshint esversion:6*/
 'use strict';
 module.exports.init = function(app, config){
 
@@ -5,7 +6,12 @@ module.exports.init = function(app, config){
         apply: function(){}
     };
 
+    app.resolve('pubsub').then(()=>{
+        app.getLogger('transform').info('Transform solved dependency.');
+    });
+
     app.getLogger('transform').info('Transform module loaded.');
 
     return transform;
 };
+module.exports.priority = -1;
