069277f40a2676e54598993f12415ce4893f78db
goliatone <burgosemi@gmail.com>
2017-09-30 22:39:25 -0400

    Add media module to example

---
+++ examples/modules/media/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports.init = function(context, config){
+    context.getLogger('media').info('Initialize media module...');
+
+    let media = {};
+    media.upload = function(data) {
+        context.getLogger('media').info('request media upload...');
+        context.emit('media.upload', data);
+    };
+
+    return new Promise(function(resolve, reject) {
+        resolve(media);
+    });
+};
