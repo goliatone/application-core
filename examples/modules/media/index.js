02f3844b415fa096698192a78328dbeea987ffe1
goliatone <burgosemi@gmail.com>
2017-09-30 22:40:27 -0400

    ../..

--- examples/modules/media/index.js
+++ examples/modules/media/index.js
@@ -4,12 +4,15 @@ module.exports.init = function(context, config){
     context.getLogger('media').info('Initialize media module...');
 
     let media = {};
-    media.upload = function(data) {
+    media.upload = function(data){
         context.getLogger('media').info('request media upload...');
         context.emit('media.upload', data);
     };
 
     return new Promise(function(resolve, reject) {
-        resolve(media);
+        //We could also register commands this way...
+        // context.loadModuleCommands(config.moduleid).then(()=>{
+            resolve(media);
+        // });
     });
 };
