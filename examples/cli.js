93dd08a8cba15ae43dce18a81e39bee84fcd66db
goliatone <burgosemi@gmail.com>
2017-10-03 23:05:15 -0400

    fix cli example

--- examples/cli.js
+++ examples/cli.js
@@ -9,12 +9,30 @@ const Application = require('..').Application;
  * - Ignore most modules, load pubusb only.
  */
 const config = Application.loadConfig({
-    coremodules: ['./logger'],
+    /*
+     * To override application-core specific
+     * configuration options we need to wrap
+     * them here.
+     */
+    app: {
+        coremodules: ['./logger'],
+        loadModulesOptions: {
+            only: ['pubsub']
+        }
+    },
+    /*
+     * Available options to override
+     * simple-config-loader:
+     * - dirname
+     * - globOptions
+     * - getConfigFiles
+     * - getPackage
+     * - configsPath
+     */
     globOptions: {
         matchPatterh: '+(app.js|pubsub.js)',
         ignorePattern: 'index.js'
-    },
-    loadModulesOptions: ['pubsub']
+    }
 }, true);
 
 const app = new Application({config});
