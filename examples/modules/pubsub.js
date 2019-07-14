8c4103c815d1af0821402aed2815fb2cc48077f6
goliatone <burgosemi@gmail.com>
2019-07-13 18:04:51 -0700

    example: remove cruft from log output

--- examples/modules/pubsub.js
+++ examples/modules/pubsub.js
@@ -1,8 +1,8 @@
-module.exports.init = function(core, config){
+module.exports.init = function(core, config) {
     var pubsub = {};
 
     return new Promise(function(resolve, reject) {
         resolve(pubsub);
-        core.getLogger('pubsub').info('Pubsub module loaded: %s', JSON.stringify(config));
+        core.getLogger('pubsub').info('Pubsub module loaded:');
     });
 };
